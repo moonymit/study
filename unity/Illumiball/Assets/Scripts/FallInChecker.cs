b25536c3f2f7d797b0d6f592c7b2e79b5daad4fb
Hi.JiGOO <hi.jigoo@gmail.com>
2016-11-04 00:53:10 +0900

    unity

---
+++ unity/Illumiball/Assets/Scripts/FallInChecker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallInChecker : MonoBehaviour {
+
+	public Hole red;
+	public Hole blue;
+	public Hole green;
+
+	void OnGUI() {
+		string label = " ";
+
+		// 모든 홀에 들어왔다면 레이블을 표시
+		if (red.IsFallIn () && blue.IsFallIn () && green.IsFallIn ()) {
+			label = "Fall in hole!";
+		}
+
+		GUI.Label (new Rect (0, 0, 100, 30), label);
+
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ unity/Illumiball/Assets/Scripts/GravityController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityController : MonoBehaviour {
+	// 중력 가속도
+	const float Gravity = 9.81f;
+
+	// 중력의 적용 상태
+	public float gravityScale = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vector = new Vector3 ();
+
+		// 유니티 에디터와 실제 기기에서의 처리를 분리
+		if (Application.isEditor) {
+
+			// 키 입력을 검출하는 벡터를 설정
+			vector.x = Input.GetAxis ("Horizontal");
+			vector.y = Input.GetAxis ("Vertical");
+
+			// 높이 방향의 판정은 z 키로 한다
+			if (Input.GetKey ("z")) {
+				vector.y = 1.0f;
+			} else {
+				vector.y = -1.0f;
+			}
+		} else {
+			// 가속도 센서의 입력을 유니티 공간의 축에 매핑한다
+			vector.x = Input.acceleration.x;
+			vector.z = Input.acceleration.y;
+			vector.y = Input.acceleration.z;
+		}
+
+		// 씬의 중력을 입력 벡터 방향에 맞추어 변화시킨다
+		Physics.gravity = Gravity * vector.normalized * gravityScale;
+	}
+}
