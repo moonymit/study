653c82259292c1eac753e9998fb671838052e86a
Hi.JiGOO <hi.jigoo@gmail.com>
2017-05-17 20:51:50 +0900

    change dir name

---
+++ deepLearning/nlp/ner.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import konlpy
+import nltk
+
+# POS tag a sentence
+sentence = u'만 6세 이하의 초등학교 취학 전 자녀를 양육하기 위해서는'
+words = konlpy.tag.Twitter().pos(sentence)
+
+# Define a chunk grammar, or chunking rules, then chunk
+grammar = "NP: {<N.*>*<Suffix>?}"
+"""
+NP: {<N.*>*<Suffix>?}   # Noun phrase 명사구
+VP: {<V.*>*}            # Verb phrase 동사구
+AP: {<A.*>*}            # Adjective phrase 형용사구
+NP: {<N.*>*<Suffix>?}   # Noun phrase
+VP: {<V.*>*}            # Verb phrase
+AP: {<A.*>*}            # Adjective phrase
+  {<DT|JJ>}          # chunk determiners and adjectives
+  }<[\.VI].*>+{      # chink any tag beginning with V, I, or .
+  <.*>}{<DT>         # split a chunk at a determiner
+  <DT|JJ>{}<NN.*>    # merge chunk ending with det/adj
+                     # with one starting with a noun
+parser = RegexpParser('''
+... NP: {<DT>? <JJ>* <NN>*} # NP
+... P: {<IN>}           # Preposition
+... V: {<V.*>}          # Verb
+... PP: {<P> <NP>}      # PP -> P NP
+... VP: {<V> <NP|PP>*}  # VP -> V (NP|PP)*
+... ''')
+{<N.*>} # 명사(N으로 시작)인 모든 경우
+{<P.*>} # 대명사(P로 시작)인 모든 경우
+{<DT><JJR>}  # DT와 JJR이 붙어 있는 경우
+{<DT><J.*>}  # DT와 J로 시작하는 단어가 붙어 있는 경우
+{<DT><JJ>?<NN>} # <DT>와 <NN>사이에 <JJ>가 있든 없든..
+"""
+parser = nltk.RegexpParser(grammar)
+chunks = parser.parse(words)
+
+print("# Print whole tree")
+print(chunks.pprint())
+
+print("\n# Print noun phrases only")
+for subtree in chunks.subtrees():
+    if subtree.label()=='NP':
+        print(' '.join((e[0] for e in list(subtree))))
+        # print(subtree.pprint())
+
+# Display the chunk tree
+chunks.draw()
